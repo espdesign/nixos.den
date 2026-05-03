f73db952e7424b5c8220500741934dabde61ae35
espdesign <evanpendergraft@gmail.com>
2026-05-03 08:58:35 -0400

    add syncthing folder

--- modules/aspects/espdesign.nix
+++ modules/aspects/espdesign.nix
@@ -39,6 +39,26 @@
             };
           };
         };
+        services.syncthing = {
+          enable = true;
+          overrideDevices = false;
+          overrideFolders = false;
+
+          settings = {
+            devices = {
+              "homelab" = {
+                id = "2B5LXHB-NE2FZ3F-M34RP6Y-G3NS2PD-JGN6CUH-KV3FWVI-ZAXEEJ5-QIZ77A3";
+              };
+            };
+            folders = {
+              "syncdocs" = {
+                id = "gfrgi-utm7w";
+                path = "/home/espdesign/Documents/syncdocs";
+                devices = [ "homelab" ];
+              };
+            };
+          };
+        };
 
       };
   };
