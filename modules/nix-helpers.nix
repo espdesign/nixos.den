76d6b861dde3737a1785546a07badbaf5a830454
espdesign <evanpendergraft@gmail.com>
2026-05-11 08:51:52 -0400

    setup cachix

--- modules/nix-helpers.nix
+++ modules/nix-helpers.nix
@@ -10,7 +10,12 @@
           nixfmt # nix formater
           nix-tree
           nix-output-monitor
+          cachix
         ];
+        nix.settings = {
+          extra-substituters = [ "https://espdesign.cachix.org" ];
+          extra-trusted-public-keys = [ "espdesign.cachix.org-1:34FPcbdU/W26YZRFCqIrVcF6uzLkcuUZvUTN5kqxebA=" ];
+        };
         programs.nh = {
           enable = true;
           clean.enable = true;
