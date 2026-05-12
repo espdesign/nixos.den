e733de05930bd9f51d8ef80e285e03840329cf3e
espdesign <evanpendergraft@gmail.com>
2026-05-12 13:24:59 -0400

    add device-ids for syncthing

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -12,6 +12,30 @@
           enable = true;
           overrideDevices = false;
           overrideFolders = false;
+          settings = {
+            devices = {
+              "hinekora" = {
+                id = "VJO634U-X3QOZCY-WFYRUQJ-UY46F6H-QB2QIFV-DUVVGGA-IA4UYYG-EKQGJAZ";
+              };
+              "kitava" = {
+                id = "SC5QXOP-IO5WX6O-UPBQHJ7-MBK52OH-BPICW2P-7NCA3PF-CFRZU6L-BD2OHQO";
+              };
+              "valako" = {
+                id = "O74L64E-XYLKVX5-5D6WKWN-C4ICOUM-JYYS3P5-PZR5N2I-UVETX2Q-O5ZAOAS";
+              };
+            };
+            folders = {
+              "syncdocs" = {
+                id = "gfrgi-utm7w";
+                path = "~/Syncthing";
+                devices = [
+                  "hinekora"
+                  "kitava"
+                  "valako"
+                ];
+              };
+            };
+          };
         };
       };
     };
