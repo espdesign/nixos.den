bb11a9ce79a2dba79909cf0d6ba0173ba103cbc3
espdesign <evanpendergraft@gmail.com>
2026-09-09 20:19:18 -0400

    add abberath omen host

---
+++ modules/hosts/abberath.nix
@@ -0,0 +1,38 @@
+# abberath is a HP OMEN 25L gaming/workstation (intel, nvidia turing)
+{
+  inputs,
+  den,
+  lib,
+  ...
+}:
+{
+  den.aspects.abberath = {
+    includes = [
+      den.provides.hostname
+
+    ];
+    provides.to-users.includes = [
+      den.aspects.gui
+      den.aspects.gnome
+      den.aspects.cli
+      den.aspects.scripts
+      den.aspects.pipewire-sound
+      den.aspects.vm
+      den.aspects.cups-print
+      den.aspects.fonts
+      den.aspects.virt-manager
+      den.aspects.rustdesk
+
+      #specific to gaming desktop
+      den.aspects.gaming
+      den.aspects.nvidia
+
+    ];
+    nixos =
+      { pkgs, ... }:
+      {
+        imports = [ ../_nixos/abberath-omen.nix ];
+        environment.systemPackages = with pkgs; [ ];
+      };
+  };
+}
---
+++ modules/users/jake.nix
@@ -0,0 +1,24 @@
+{
+  den,
+  ...
+}:
+{
+  den.aspects.jake = {
+    includes = [
+      den.provides.define-user
+      (den.provides.user-shell "zsh")
+    ];
+    nixos =
+      { ... }:
+      {
+        users.users.jake = {
+          initialHashedPassword = "$6$nswNImVPvYzggm7T$sWRM8e0fWMWs007Y8QlH10dysg5/ILPms.4yJ.sUjBq93o5.h7.7GkVmzkpoLQlqrkdugWixAnchrN4UNkRaS.";
+        };
+      };
+
+    homeManager =
+      { ... }:
+      {
+      };
+  };
+}
